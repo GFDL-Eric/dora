1b936db26d39f15c058b1bd107e1d079760ab95d
Eric Stofferahn <Eric.Stofferahn@noaa.gov>
2024-04-01 14:16:13 -0400

    moved testing system (dora.gfdl.noaa.gov:5600) to dora server [skip ci]

--- generate-ci-config.py
+++ generate-ci-config.py
@@ -5,19 +5,26 @@ mode = os.environ['mode']
 print(mode)
 
 my_yaml = {'image': 'docker/compose:latest',
+           'tags': ['dora'],
+           'environment': 'production',
            'stages': []}
 
-cfg = {'test': {'tags': ['dpdev'], 'environment': 'testing',
-                'secret_id': 'google_id_env_ci', 'secret_secret': 'google_secret_env_ci'},
-       'dora': {'tags': ['dora'], 'environment': 'production',
-                'secret_id': 'google_id_env_dora', 'secret_secret': 'google_secret_env_dora'}}
+secrets = {'secret_id': 'google_id_env_dora',
+           'secret_secret': 'google_secret_env_dora',
+           'secret_id_test': 'google_id_env_ci',
+           'secret_secret_test': 'google_secret_env_ci'}
 
 def build_script(action, in_pipe, in_full):
   dockerprefix = f'docker-compose -f docker-compose.{in_pipe}.yml'
-  my_env = cfg[in_pipe]['environment']
-  my_machine = cfg[in_pipe]['tags'][0]
-  secret_id = cfg[in_pipe]['secret_id']
-  secret_secret = cfg[in_pipe]['secret_secret']
+  suffix=''
+  if 'test' in in_pipe:
+    suffix='test'
+  my_env = my_yaml['environment']
+  my_machine = my_yaml['tags'][0]
+  secret_id = secrets['secret_id']
+  secret_secret = secrets['secret_secret']
+  secret_id_test = secrets['secret_id_test']
+  secret_secret_test = secrets['secret_secret_test']
   def make_before_statement(in_action, in_service):
     return [f'echo "{in_action.capitalize()}ing the {my_env} {in_service} on {my_machine}..."']
   def make_after_statement(in_action_past, in_service):
@@ -27,7 +34,13 @@ def build_script(action, in_pipe, in_full):
     case "shutdown":
       match in_full:
         case "webapp":
-          return [f'{dockerprefix} rm -f -s webapp']
+          return [f'{dockerprefix} rm -f -s webapp{suffix}']
+        case "full":
+          return [f'{dockerprefix} down']
+    case "stop":
+      match in_full:
+        case "webapp":
+          return [f'{dockerprefix} stop webapp{suffix}']
         case "full":
           return [f'{dockerprefix} down']
     case "build":
@@ -35,7 +48,7 @@ def build_script(action, in_pipe, in_full):
         case "webapp":
           build_before_statement = make_before_statement('build', 'webapp')
           build_after_statement = make_after_statement('built on', 'webapp')
-          build_statement = [f'{dockerprefix} build webapp']
+          build_statement = [f'{dockerprefix} build webapp{suffix}']
         case "full":
           build_before_statement = make_before_statement('build', 'webapp and database application')
           build_after_statement = make_after_statement('built on', 'webapp and database application')
@@ -43,18 +56,24 @@ def build_script(action, in_pipe, in_full):
           build_statement += ['docker cp mytmpsource:/source/backup.sql /builds/john.krasting/dora/mariadb/']
           build_statement += ['docker stop mytmpsource']
           build_statement += ['docker rm mytmpsource']
-          build_statement += [f'{dockerprefix} build']
+          build_statement += [f'{dockerprefix} build mariadb{suffix}']
       script += build_before_statement
-      script += ['sed -i \'s/google_id_env_ci/\'${' + secret_id + '}\'/\' .env']
-      script += ['sed -i \'s/google_secret_env_ci/\'${' + secret_secret + '}\'/\' .env']
-      match in_pipe:
-        case "test":
-          script += ['sed -i \'/certfile/s/cert\.pem/gfdl.noaa.gov.crt/\' gunicorn/gunicorn-run.sh']
-          script += ['sed -i \'/keyfile/s/key\.pem/gfdl.noaa.gov.key/\' gunicorn/gunicorn-run.sh']
+      if 'test' in in_pipe:
+        script += ['sed -i \'s/google_id_env_ci/\'${' + secret_id_test + '}\'/\' .env']
+        script += ['sed -i \'s/google_secret_env_ci/\'${' + secret_secret_test + '}\'/\' .env']
+      else:
+        script += ['sed -i \'s/google_id_env_ci/\'${' + secret_id + '}\'/\' .env']
+        script += ['sed -i \'s/google_secret_env_ci/\'${' + secret_secret + '}\'/\' .env']
       script += build_statement
       script += build_after_statement
       return script
       
+    case "start":
+      script += make_before_statement('start', 'webapp')
+      script += [f'{dockerprefix} start webapp{suffix}']
+      script += make_after_statement('restarted on', 'webapp')
+      return script
+
     case "deploy":
       script += make_before_statement('deploy', 'webapp and database application')
       script += [f'{dockerprefix} up -d']
@@ -64,9 +83,7 @@ def build_script(action, in_pipe, in_full):
 def build_job_dict(action, in_yaml, in_pipeline, full='webapp'):
   in_yaml['stages'].append(action)
   in_yaml[f'{action}-job'] = {'stage': action, 'script': build_script(action, in_pipeline, full)}
-  in_yaml[f'{action}-job'] = in_yaml[f'{action}-job'] | cfg[in_pipeline] 
-  del in_yaml[f'{action}-job']['secret_id'] 
-  del in_yaml[f'{action}-job']['secret_secret'] 
+  in_yaml[f'{action}-job'] = {k:my_yaml[k] for k in ['environment', 'image', 'tags']} | in_yaml[f'{action}-job']
   return in_yaml
   
 match mode.split('-'):
@@ -80,9 +97,9 @@ match mode.split('-'):
       my_yaml = build_job_dict('build', my_yaml, pipeline, full='full')
   case ["deploy", pipeline]:
       my_yaml = build_job_dict('deploy', my_yaml, pipeline)
-  case ["downandup", pipeline]:
-      my_yaml = build_job_dict('shutdown', my_yaml, pipeline)
-      my_yaml = build_job_dict('deploy', my_yaml, pipeline)
+  case ["reset", pipeline]:
+      my_yaml = build_job_dict('stop', my_yaml, pipeline)
+      my_yaml = build_job_dict('start', my_yaml, pipeline)
   case ["standard", pipeline]:
       my_yaml = build_job_dict('shutdown', my_yaml, pipeline)
       my_yaml = build_job_dict('build', my_yaml, pipeline)
@@ -95,5 +112,9 @@ match mode.split('-'):
     raise KeyError('Unidentified mode provided')
 
 with open('generated-config.yml','w') as fh:
-  yaml.dump(my_yaml,fh,default_flow_style=False)
+  out_yaml = {}
+  out_yaml['stages'] = my_yaml['stages']
+  for stage in out_yaml['stages']:
+    out_yaml[f'{stage}-job'] = my_yaml[f'{stage}-job']
+  yaml.dump(out_yaml,fh,default_flow_style=False)
 
